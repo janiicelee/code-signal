fd0037ea4ff3a826c9a587c7eb4d2e34be917556
Janice Lee <meekukin@gmail.com>
2022-03-13 00:09:55 -0800

    Solution for all-longest-strings.js

---
+++ smooth-sailing/all-longest-strings.js
@@ -0,0 +1,33 @@
+/*
+
+input: array
+output: array
+
+make a new storage
+loop over the input array
+find the longest string in the array
+put the longest string in the new storage
+check if the other strings has the same length with the longest string
+also put them in the new storage
+return the storage
+*/
+
+
+function solution(inputArray) {
+  debugger;
+  var longestStrings = [];
+  var longest = 0;
+
+  for(var string of inputArray) {
+    if (string.length > longest) {
+      //initialize storage when the for loop starts
+      longestStrings= [];
+      longest = string.length;
+      longestStrings.push(string);
+    } else if ( string.length === longest)
+      longestStrings.push(string);
+    }
+  return longestStrings;
+}
+
+solution(["ab", "aad", "ad", "vcd", "aba"]);
